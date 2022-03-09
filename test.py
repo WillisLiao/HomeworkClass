984398c0210940bada10fc51c0a81445a468873a
Willis Liao <willisrain04@gmail.com>
2022-03-10 01:46:51 +0800

    ok

--- test.py
+++ test.py
@@ -1,8 +1,14 @@
 import os
-save_path = 'C:\{}'.format(input('書你的路徑Users\willi\HomeworkClass'))
-file_name = 'test.txt'
-completeName = os.path.join(save_path, file_name)
-print(completeName)
-file1 = open(completeName, "w")
-file1.write("fk you")
-file1.close()
+#####3
+#with open("copy.txt", "w") as file:
+#    file.write("Your text goes here")
+#########################################
+for i in range(1, 30):
+
+    save_path = 'C:\{}'.format(input('輸入你的路徑Users\willi\HomeworkClass'))
+    file_name = 'char{}.txt'.format(i)
+    completeName = os.path.join(save_path, file_name)
+    print(completeName)
+    file1 = open(completeName, "w")
+    file1.write("fk you")
+    file1.close()
